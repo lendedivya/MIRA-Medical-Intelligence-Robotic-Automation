4ed57f92f5ec6b22c6364f649826f516b80226a3
Divya Lende <154407173+lendedivya@users.noreply.github.com>
2026-06-06 18:28:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -265,4 +265,4 @@ def delete_patient(pid):
 # ────────────────────────────────────────────────────────
 if __name__ == "__main__":
     init_db()
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
